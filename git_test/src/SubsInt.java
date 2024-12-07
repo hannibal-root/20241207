9e42a7d618454567ae2d2fe01cbe8cf59fd101a0
monik <monika.menyhart@yahoo.com>
2024-12-07 13:52:24 +0100

    Ezt mindig írni kell

---
+++ git_test/src/SubsInt.java
@@ -0,0 +1,11 @@
+public class SubsInt {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 7;
+    }
+
+    int subsInt (int a, int b) {
+        return (a - b);
+    }
+
+}
