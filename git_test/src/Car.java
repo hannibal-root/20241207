005dd64dfc6360c809524975b1fa540a5b250255
Your Name <a@b.com>
2024-12-07 14:27:54 +0100

    Hozzáadtam egy scannert.

--- git_test/src/Car.java
+++ git_test/src/Car.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+import java.util.scanner;
 public class Car {
     public static void main(String[] args) {
         System.out.println("Ford");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adj meg egy szöveget.");
+        String textOut = sc.nextLine();
+        System.out.println(textOut);
     }
 }
