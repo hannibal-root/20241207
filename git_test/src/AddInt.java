40197d13276fc901cd8e95e1a019dd2f64f08002
KuruczMartin <soneec6@gmail.com>
2024-12-07 15:17:29 +0100

    Prinw power1

--- git_test/src/AddInt.java
+++ git_test/src/AddInt.java
@@ -3,14 +3,22 @@ public class AddInt {
         int sum = add(5, 6, 10); // Should be 21
 
         System.out.println(sum);
-        System.out.println(neg(5,6,10));
+        System.out.println(neg(5, 6, 10));
     }
 
     public static int add(int a, int b, int c) {
         return a + b + c;
     }
 
-    public static int neg(int a, int b, int c){
+    public static int neg(int a, int b, int c) {
         return a + (b * -1) + c;
+
+        public static int  ( int a, int b){
+            if (b == 0) {
+                System.out.println("Error: Division by zero is not allowed.");
+                return 0;
+            }
+            return a / b;
+        }
     }
 }
