e70d310f75b9148df254180aecd9297870dc0070
Gergő <zeleig96@gmail.com>
2024-12-07 15:24:12 +0100

    refakt

--- git_test/src/AddInt.java
+++ git_test/src/AddInt.java
@@ -3,13 +3,9 @@ public class AddInt {
         int sum = add(5, 6, 10); // Should be 21
 
         System.out.println(sum);
- 
-  
-  
         System.out.println(neg(5, 6, 10));
-
-        System.out.println(neg(5,6,10));
-        System.out.println(power (5, 4));
+        System.out.println(div(5, 6));
+        System.out.println(power(5, 4));
 
     }
 
@@ -20,16 +16,18 @@ public class AddInt {
     public static int neg(int a, int b, int c) {
         return a + (b * -1) + c;
 
-        public static int  ( int a, int b){
-            if (b == 0) {
-                System.out.println("Error: Division by zero is not allowed.");
-                return 0;
-            }
-            return a / b;
-        }
     }
 
-    public static int power(int a, int b){
+    public static int div(int a, int b) {
+        if (b == 0) {
+            System.out.println("Error: Division by zero is not allowed.");
+            return 0;
+        }
+        return a / b;
+    }
+
+    public static int power(int a, int b) {
         return a * b;
     }
+
 }
