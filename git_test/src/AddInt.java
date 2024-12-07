09532831c18726d694db8c952ad9ad587e167028
Masterfield <kisvarga.gabor@masterfield.hu>
2024-12-07 14:57:03 +0100

    AddInt issue

--- git_test/src/AddInt.java
+++ git_test/src/AddInt.java
@@ -1,5 +1,11 @@
 public class AddInt {
-    static int add(int a, int b, int c) {
-        return a + b + c;
+    public static void main(String[] args) {
+        int sum = add(5, 6, 10); // Should be 21
+
+        System.out.println(sum);
+    }
+
+    public static int add(int a, int b, int c) {
+        return a + b - c;
     }
 }
